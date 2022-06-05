a24543fb47ce6241fe82709aeea813b2398c3f26
marcadamsge <marc.adams.ge@gmail.com>
2022-06-05 17:38:49 +0200

    expose the StepOrCreate method

--- trie/trie.go
+++ trie/trie.go
@@ -12,10 +12,15 @@ func New[T any]() *Trie[T] {
 	}
 }
 
+// Insert a string into the trie. str may be empty, in which case the value of the current trie is updated.
+// The combineValues function merges the value of the trie being updated with the new value, this is a way
+// to avoid overwriting previously inserted values.
+// This function is pretty basic, if you need more control over how the values are inserted
+// (like storing prefixes as well for example), it's better to use StepOrCreate directly.
 func (trie *Trie[T]) Insert(str string, value *T, combineValues func(t1 *T, t2 *T) *T) {
 	crtTrie := trie
 	for _, r := range []rune(str) {
-		crtTrie = crtTrie.stepOrCreate(r)
+		crtTrie = crtTrie.StepOrCreate(r)
 	}
 	crtTrie.Value = combineValues(crtTrie.Value, value)
 }
@@ -25,13 +30,8 @@ func (trie *Trie[T]) Step(r rune) *Trie[T] {
 	return trie.children[r]
 }
 
-func (trie *Trie[T]) Iterate(iterationFunction func(r rune, trie *Trie[T])) {
-	for r, tr := range trie.children {
-		iterationFunction(r, tr)
-	}
-}
-
-func (trie *Trie[T]) stepOrCreate(r rune) *Trie[T] {
+// StepOrCreate the next Trie if it does not exist.
+func (trie *Trie[T]) StepOrCreate(r rune) *Trie[T] {
 	step, ok := trie.children[r]
 	if ok {
 		return step
@@ -43,4 +43,11 @@ func (trie *Trie[T]) stepOrCreate(r rune) *Trie[T] {
 	}
 	trie.children[r] = out
 	return out
+}
+
+// Iterate over all the children of this trie.
+func (trie *Trie[T]) Iterate(iterationFunction func(r rune, trie *Trie[T])) {
+	for r, tr := range trie.children {
+		iterationFunction(r, tr)
+	}
 }
